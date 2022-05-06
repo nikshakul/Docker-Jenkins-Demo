9c109a77a18076f81953c0fb0e5df83dece537fa
First Last <nikhilshankarku@ip-172-31-23-250.ec2.internal>
2022-05-06 19:43:02 +0000

    Committing a change to add draft tf file for creating first vm instance

---
+++ creds.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+   access_key = "ASIAQL7J7UY4FH3QXBVC"
+   secret_key = "fpGcHw1guAjZd6dpYdRdjmtnbLp8E6bDGQDqx4q2"
+   token = "FwoGZXIvYXdzEO3//////////wEaDKNFohoGeAQ9W07KFiLAARwJx7YCZpqbOxe9RCP1CYG4DNE2efD/wcmopBpqzZW5d35pIqtF4vBPG9rjtVLg8+JiEG+79P3e4zuszhxqZYyQXv8DF1yKDxGqZa6H+P9O70uLhOIfm/3JAlifxcmPfzC1AJ3B+eAvq2ex5LNV2L1JJJysCOj2L1HECvJkfc23FV70Od/gYUnhVGwkH/KiO8fGcUCj0Pbf8YyvtIAB75JFbJKsLh+O+nVFaHJa5C8rZkelkPM5wBtP6e/UZEsy0CiCkdSTBjIt2WrdZxJfB4a1U6IESu/H9sDbNDPszn9jW3m/gGnwuKVzFGnYEGBD8ZNQmvl4"
+   region = "us-east-1"
+}
